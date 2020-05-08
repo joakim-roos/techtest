a49e95bae8fded4b66ff65bcec2e56f3b39bdb32
Joakim Roos <joakim.roos@student.kyh.se>
2020-05-08 12:49:28 +0200

    added exports for assets

--- src/components/Salon/index.js
+++ src/components/Salon/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { SalonImage } from '../../assets/images'
+import { IMAGE, IMAGE2X, IMAGE3X } from '../../constants/assets'
 
+export const SalonImage = () => (
+  <img
+    srcSet={`
+      ${IMAGE3X} 3x,
+      ${IMAGE2X} 2x,
+      ${IMAGE} 1x,
+      `}
+    src={IMAGE3X}
+    alt='A womans long hair is caught by the wind'
+  >
+  </img>
+)
 
 function Salon(props) {
   let { slug } = useParams()
