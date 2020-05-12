1728ce6947b8678929983e15ba6d9538585c232c
Joakim Roos <joakim.roos@student.kyh.se>
2020-05-12 20:17:16 +0200

    updated readme

--- src/App.js
+++ src/App.js
@@ -7,20 +7,8 @@ import SalonList from './components/SalonList'
 
 import { API_URL, SALONS } from '././constants/routes'
 
-/* 
-  I tried to make the application as close to the assets provided as possible, checking font-sizes, paddings etc. 
-
-  All desired functions should work.
-
-  Please be aware that I made this application for mobiles first, so it does look a bit wierd when using browser :)  
-
-  I really hope you like it!
-*/
-
 function App() {
   const [allSalons, setAllSalons] = useState([])
-
-
   // Fetches all salon-data when the component mounts. 
   // Adds the data to the allSalons-state. 
   useEffect(() => {
@@ -37,25 +25,17 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-
-          <Route exact path="/salons/:slug">
-            <Salon />
-          </Route>
-
-          <Route exact path="/salons">
-            <SalonList
-              allSalons={allSalons}
-            />
-          </Route>
-
-          <Redirect from='/' to="/salons" />
-
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/salons/:slug">
+          <Salon />
+        </Route>
+        <Route exact path="/salons">
+          <SalonList allSalons={allSalons} />
+        </Route>
+        <Redirect from='/' to="/salons" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
